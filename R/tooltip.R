1c13499b05aa632748bcdd4c15174dd82ef4593f
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-09-17 13:26:01 +0200

    complete function returning detailed episode info

---
+++ R/tooltip.R
@@ -0,0 +1,37 @@
+#' Text info about given episode.
+#'
+#' @param filtData Data frame returned by filterToPlot function.
+#' @param click shiny object returned on plot click.
+#' @param typeRating chr, "imdbRating" or "nielsenRating".
+#'
+#' @return html with show name, episode's signature, episode's title,
+#'         rating and number of votes/numbers of viewers, air date in 
+#'         separate lines.
+#'
+#' @export
+#'
+
+tooltip <- function(filtData, click, typeRating) {
+  if(is.null(click)) {
+    return("Click a point to display details about the episode. ")
+  } else {
+    point <- nearPoints(filtData, click,  xvar = "airDate", yvar = "rating")
+    tooltipText <- paste(paste("Episode details:", point[["showTitle"]], 
+			       paste(point[["episode"]], point[["season"]], sep = "x")),
+			 paste("Title:", point[["epTitle"]]),
+			 paste("Aired:", point[["airDate"]]),
+			 paste("Rating:", round(as.numeric(point[["rating"]]), 2)),
+			 sep = "<br />")
+    if(typeRating == "imdbRating") {
+      tooltipText <- paste(tooltipText,
+			   paste("Number of votes:", point[["numOfVotes"]]),
+			   sep = "<br />")
+
+    } else {
+      tooltipText <- paste(tooltipText,
+			   paste("Viewers in milions:", point[["viewers"]]),
+			   sep = "<br />")
+    }
+  }
+  return(HTML(tooltipText))
+}
