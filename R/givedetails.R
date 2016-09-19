8ffa09fda5ba7e6b5503abd805f9affe5ca99a37
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-09-20 00:35:54 +0200

    number of other show is now displayed

--- R/givedetails.R
+++ R/givedetails.R
@@ -19,15 +19,15 @@ giveDetails <- function(filtData, click, typeRating, netflix = FALSE) {
     x <- "ep"
   }
   if(is.null(click)) {
-    tooltipText <- "Click a point to display details about the episode."
+    tooltipText <- "Click a single point to display details about the episode."
   } else {
-    point <- nearPoints(filtData, click,  xvar = x, yvar = "rating")
+    point <- nearPoints(filtData[[2]], click,  xvar = x, yvar = "rating")
     if(dim(point)[1] == 0) {
       tooltipText <- "Click a point to display details about the episode."
     } else {
-      tooltipText <- paste(paste("Episode details:", point[["showTitle"]], 
-				 paste(point[["season"]], point[["episode"]], sep = "x")),
-			   paste("Title:", point[["epTitle"]]),
+      tooltipText <- paste(paste(point[["showTitle"]], 
+				 paste(point[["season"]], point[["episode"]], sep = "x"),
+				 point[["epTitle"]]),
 			   paste("Aired:", point[["airDate"]]),
 			   paste("Rating:", round(as.numeric(point[["rating"]]), 2)),
 			   sep = "<br />")
@@ -40,6 +40,19 @@ giveDetails <- function(filtData, click, typeRating, netflix = FALSE) {
 	tooltipText <- paste(tooltipText,
 			     paste("Viewers in milions:", point[["viewers"]]),
 			     sep = "<br />")
+      }
+      if(!netflix) {
+	filtData[[1]] %>%
+	  filter(datePlot == point[["airDate"]]) -> greyLineInfo
+	if(dim(greyLineInfo)[1] != 0) {
+	  form <- "shows"
+	  if(greyLineInfo[["noOfShows"]] == 1) {
+	    form <- "show"
+	  } 
+	  tooltipText <- paste(tooltipText, 
+			       paste(greyLineInfo[["noOfShows"]], "other", form, "aired that week."),
+			       sep = "<br />")
+	}
       }
     }
   }
