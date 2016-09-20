db0d87ecc097a3f883a842d1376d395ec6cb0196
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-09-20 12:10:44 +0200

    first step in fixing labels

--- R/givedetails.R
+++ R/givedetails.R
@@ -13,18 +13,20 @@
 #'
 
 giveDetails <- function(filtData, click, typeRating, netflix = FALSE) {
+  sourceData <- filtData
   tooltipText <- ""
-  x <- "airDate"
-  if(netflix) {
-    x <- "ep"
+  x <- "ep"
+  if(!netflix) {
+    x <- "airDate"
+    sourceData <- filtData[[2]]
   }
   if(is.null(click)) {
-    tooltipText <- "Click a single point to display details about the episode."
+    tooltipText <- "Click a point to display details about the episode."
   } else {
-    point <- nearPoints(filtData[[2]], click,  xvar = x, yvar = "rating")
+    point <- nearPoints(sourceData, click,  xvar = x, yvar = "rating")
     if(dim(point)[1] == 0) {
       tooltipText <- "Click a point to display details about the episode."
-    } else {
+    } else if(dim(point)[1] == 1){
       tooltipText <- paste(paste(point[["showTitle"]], 
 				 paste(point[["season"]], point[["episode"]], sep = "x"),
 				 point[["epTitle"]]),
@@ -54,7 +56,46 @@ giveDetails <- function(filtData, click, typeRating, netflix = FALSE) {
 			       sep = "<br />")
 	}
       }
+    } else {
+      tooltipText <- "Click on a single point to display details about the episode."
     }
-  }
+  } 
   return(HTML(tooltipText))
 }
+
+# 
+# #' Display shorter episode info under comparison plot.
+# #'
+# #' @param filData Data frame returned by filterToPlot or filterNetflix function.
+# #' @param click Shiny on_click object.
+# #' @param netflix lgl, if TRUE "ep" variable will be used as xvar.
+# #'
+# #' @return character with show title, episode signature and title.
+# #'
+# #' @export
+# #'
+# 
+# giveShortDetails <- function(filtData, click, netflix) {
+#   sourceData <- filtData
+#   tooltipText <- ""
+#   x <- "airDate"
+#   if(netflix) {
+#     x <- "ep"
+#   } else {
+#     sourceData <- filtData[[2]]
+#   }
+#   if(is.null(click)) {
+#     tooltipText <- "Click on a point to display episode info."
+#   }
+#   else {
+#     point <- nearPoints(sourceData, click,  xvar = x, yvar = "rating")
+#     if(dim(point)[1] == 1) {
+#       tooltipText <- paste(point[["showTitle"]], 
+# 			   paste(point[["season"]], point[["episode"]], sep = "x"),
+# 			   point[["epTitle"]])
+#     } else if(dim(point)[1] > 1) {
+#       tooltipText <- "Click on a single point to display episode info."
+#     }
+#   }
+#   return(tooltipText)
+# }
